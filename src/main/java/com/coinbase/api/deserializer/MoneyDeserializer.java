3f26b2634fa5aca5fdd47e4fc1f603f78c34a3b5
Alex Ianus <aianus@coinbase.com>
2014-06-11 18:04:47 +0700

    Indentation and support cents

--- src/main/java/com/coinbase/api/deserializer/MoneyDeserializer.java
+++ src/main/java/com/coinbase/api/deserializer/MoneyDeserializer.java
@@ -15,44 +15,52 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
 
 public class MoneyDeserializer extends StdDeserializer<Money> {
-	
-	public MoneyDeserializer() {
-		super(Money.class);
+
+    public MoneyDeserializer() {
+	super(Money.class);
+    }
+
+    @Override
+    public Money deserialize(JsonParser jsonParser, DeserializationContext deserializationContext)
+	    throws IOException {
+
+	Money result = null;
+
+	ObjectCodec oc = jsonParser.getCodec();
+	JsonNode node = oc.readTree(jsonParser);
+
+	String currency = null;
+	String amount = null;
+	Long cents = null;
+
+	if (node.has("currency")) {
+	    currency = node.get("currency").textValue();
+	} else if (node.has("currency_iso")) {
+	    currency = node.get("currency_iso").textValue();
 	}
-	
-	@Override
-	public Money deserialize(JsonParser jsonParser, 
-	            DeserializationContext deserializationContext) throws IOException {
-	
-			Money result = null;
-		
-	        ObjectCodec oc = jsonParser.getCodec();
-	        JsonNode node = oc.readTree(jsonParser);
-	        
-	        String currency = null;
-	        String amount= null;
-	        
-	        if (node.has("currency")) {
-	        	currency = node.get("currency").textValue();
-	        } else if (node.has("currency_iso")) {
-	        	currency = node.get("currency_iso").textValue();
-	        }
-	        
-	        if (node.has("amount")) {
-	        	amount = node.get("amount").textValue();
-	        }
-	        
-	        if (currency == null || amount == null) {
-	        	throw new JsonParseException("Wrong format for Money", jsonParser.getCurrentLocation());
-	        }
-	        
-	        try {
-	        	result = Money.of(CurrencyUnit.of(currency), new BigDecimal(amount));
-	        } catch (Exception ex) {
-	        	throw new JsonParseException("Could not construct Money from arguments", jsonParser.getCurrentLocation(), ex);
-	        }
-	        
-	        return result;
-	        	
+
+	if (node.has("amount")) {
+	    amount = node.get("amount").textValue();
+	} else if (node.has("cents")) {
+	    cents = node.get("cents").longValue();
 	}
+
+	if (currency == null || (amount == null && cents == null)) {
+	    throw new JsonParseException("Wrong format for Money", jsonParser.getCurrentLocation());
+	}
+
+	try {
+	    if (amount != null) {
+		result = Money.of(CurrencyUnit.of(currency), new BigDecimal(amount));
+	    } else {
+		result = Money.ofMinor(CurrencyUnit.of(currency), cents);
+	    }
+	} catch (Exception ex) {
+	    throw new JsonParseException("Could not construct Money from arguments",
+		    jsonParser.getCurrentLocation(), ex);
+	}
+
+	return result;
+
+    }
 }
--- src/test/java/com/coinbase/api/deserializer/MoneyDeserializerTest.java
+++ src/test/java/com/coinbase/api/deserializer/MoneyDeserializerTest.java
@@ -15,40 +15,54 @@ import com.fasterxml.jackson.databind.module.SimpleModule;
 
 public class MoneyDeserializerTest {
 
-	private ObjectMapper mapper;
-	
-	@Before
+    private ObjectMapper mapper;
+
+    @Before
     public void setUp() {
-		mapper = new ObjectMapper();
-		SimpleModule module = new SimpleModule();
-		module.addDeserializer(Money.class, new MoneyDeserializer());
-		mapper.registerModule(module);
+	mapper = new ObjectMapper();
+	SimpleModule module = new SimpleModule();
+	module.addDeserializer(Money.class, new MoneyDeserializer());
+	mapper.registerModule(module);
     }
-	
-	@Test
-	public void usd() throws Exception {
-		String json = "{\"amount\": \"49.76\", \"currency\": \"USD\"}";
-		Money money = mapper.readValue(json, Money.class);
-		assertEquals(Money.parse("USD 49.76"), money);
-	}
-	
-	@Test
-	public void btc() throws Exception {
-		String json = "{\"amount\": \"49.76987601\", \"currency\": \"BTC\"}";
-		Money money = mapper.readValue(json, Money.class);
-		assertEquals(Money.of(CurrencyUnit.of("BTC"), new BigDecimal("49.76987601")), money);
-	}
-	
-	@Test(expected = JsonParseException.class)
-	public void invalidJson() throws Exception {
-		String json = "{\"name\": \"Alex\", \"Nationality\": \"Canadian\"}";
-		Money money = mapper.readValue(json, Money.class);
-	}
-	
-	@Test(expected = JsonParseException.class)
-	public void unknownCurrency() throws Exception {
-		String json = "{\"amount\": \"49.76987601\", \"currency\": \"WTF\"}";
-		Money money = mapper.readValue(json, Money.class);
-	}
+
+    @Test
+    public void usd() throws Exception {
+	String json = "{\"amount\": \"49.76\", \"currency\": \"USD\"}";
+	Money money = mapper.readValue(json, Money.class);
+	assertEquals(Money.parse("USD 49.76"), money);
+    }
+
+    @Test
+    public void btc() throws Exception {
+	String json = "{\"amount\": \"49.76987601\", \"currency\": \"BTC\"}";
+	Money money = mapper.readValue(json, Money.class);
+	assertEquals(Money.of(CurrencyUnit.of("BTC"), new BigDecimal("49.76987601")), money);
+    }
+
+    @Test
+    public void btc_iso_code() throws Exception {
+	String json = "{\"amount\": \"49.76987601\", \"currency_iso\": \"BTC\"}";
+	Money money = mapper.readValue(json, Money.class);
+	assertEquals(Money.of(CurrencyUnit.of("BTC"), new BigDecimal("49.76987601")), money);
+    }
+
+    @Test
+    public void btc_long_cents() throws Exception {
+	String json = "{\"cents\": 4976987601, \"currency_iso\": \"BTC\"}";
+	Money money = mapper.readValue(json, Money.class);
+	assertEquals(Money.of(CurrencyUnit.of("BTC"), new BigDecimal("49.76987601")), money);
+    }
+
+    @Test(expected = JsonParseException.class)
+    public void invalidJson() throws Exception {
+	String json = "{\"name\": \"Alex\", \"Nationality\": \"Canadian\"}";
+	Money money = mapper.readValue(json, Money.class);
+    }
+
+    @Test(expected = JsonParseException.class)
+    public void unknownCurrency() throws Exception {
+	String json = "{\"amount\": \"49.76987601\", \"currency\": \"WTF\"}";
+	Money money = mapper.readValue(json, Money.class);
+    }
 
 }
