5ad7342c6c4af3dd6def32e9ce9aafadc3fcd08a
anujmiddha <anuj.middha@gmail.com>
2014-12-05 17:34:50 +0530

    Added oauth tokens to UserResponse

--- src/main/java/com/coinbase/api/entity/UserResponse.java
+++ src/main/java/com/coinbase/api/entity/UserResponse.java
@@ -2,10 +2,11 @@ package com.coinbase.api.entity;
 
 public class UserResponse extends Response {
     /**
-     * 
+     *
      */
     private static final long serialVersionUID = 8847695815066590925L;
     private User _user;
+    private OAuthTokensResponse _oauth;
 
     public User getUser() {
         return _user;
@@ -15,4 +16,11 @@ public class UserResponse extends Response {
         _user = user;
     }
 
+    public OAuthTokensResponse getOauth() {
+        return _oauth;
+    }
+
+    public void setOauth(OAuthTokensResponse oauth) {
+        this._oauth = oauth;
+    }
 }
