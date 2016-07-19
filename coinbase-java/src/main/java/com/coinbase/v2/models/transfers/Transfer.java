ebfc425b48740808e603e1480ba558d3759c66ef
janechung <jane@coinbase.com>
2016-07-19 10:49:07 -0700

    Added warnings to transfer objects

--- coinbase-java/src/main/java/com/coinbase/v2/models/transfers/Transfer.java
+++ coinbase-java/src/main/java/com/coinbase/v2/models/transfers/Transfer.java
@@ -4,6 +4,9 @@ package com.coinbase.v2.models.transfers;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.annotation.Generated;
 
 @Generated("org.jsonschema2pojo")
@@ -12,6 +15,9 @@ public class Transfer {
     @SerializedName("data")
     @Expose
     private Data data;
+    @SerializedName("user_warnings")
+    @Expose
+    private List<UserWarning> userWarnings = new ArrayList<UserWarning>();
 
     /**
      * 
@@ -29,6 +35,24 @@ public class Transfer {
      */
     public void setData(Data data) {
         this.data = data;
+    }
+
+    /**
+     *
+     * @return
+     *     The userWarnings
+     */
+    public List<UserWarning> getUserWarnings() {
+        return userWarnings;
+    }
+
+    /**
+     *
+     * @param userWarnings
+     *     The user_warnings
+     */
+    public void setUserWarnings(List<UserWarning> userWarnings) {
+        this.userWarnings = userWarnings;
     }
 
 }
