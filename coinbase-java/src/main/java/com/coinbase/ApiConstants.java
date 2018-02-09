18ad3bb67815600b5b2a22a86e94cecbb32fbb59
Dianne Askew <dianne.askew@coinbase.com>
2018-02-09 10:47:14 -0800

    bump api version

--- coinbase-java/src/main/java/com/coinbase/ApiConstants.java
+++ coinbase-java/src/main/java/com/coinbase/ApiConstants.java
@@ -4,7 +4,7 @@ public class ApiConstants {
     public static final String BASE_URL_PRODUCTION = "https://api.coinbase.com";
     public static final String SERVER_VERSION = "v2";
     public static final String CLIENT_PLATFORM = "mobile";
-    public static final String VERSION = "2018-01-22";
+    public static final String VERSION = "2018-02-08";
 
     // Endpoints
     public static final String TOKEN = "token";
